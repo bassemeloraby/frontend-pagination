123f2c55b783c0427ed221feaa2d1fefb1f31644
bassem eloraby <BASEM12683@GMAIL.COM>
2023-01-06 14:40:59 +0300

    done for pagination

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Posts from './components/Posts';
+import Pagination from './components/Pagination';
 function App() {
   const [posts, setPosts] = useState([]);
- 
+
   //start page
   const [currentPage, setCurrentPage] = useState(1);
   //number of posts
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(5);
 
   //fetch data
   useEffect(() => {
@@ -19,16 +20,23 @@ function App() {
     fetchPosts();
   }, []);
 
-// Get current posts
-const indexOfLastPost = currentPage * postsPerPage;
-const indexOfFirstPost = indexOfLastPost - postsPerPage;
-const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
       <Posts posts={currentPosts} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
